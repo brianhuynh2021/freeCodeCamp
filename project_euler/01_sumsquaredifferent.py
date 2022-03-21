5c8c254828091e478b5dde34c8b7c876d81f035a
brian huynh <huynh2102@gmail.com>
2022-03-21 11:29:52 +0700

    update code

--- project_euler/01_sumsquaredifferent.py
+++ project_euler/01_sumsquaredifferent.py
@@ -13,3 +13,18 @@ def sumsquaredifferent(n):
 
 result = sumsquaredifferent(100)
 print(result)
+
+# -------------------------------------
+# For javascript
+# function sumSquareDifference(n) {
+#   let sum1 = 0;
+#   let sum2 = 0;
+#   for(let i = 1;i<=n;i++){
+#     sum1 += i*i;
+#     sum2 += i;
+#   }
+#   sum2 = sum2*sum2;
+#   return sum2-sum1;
+# }
+
+# sumSquareDifference(100);
