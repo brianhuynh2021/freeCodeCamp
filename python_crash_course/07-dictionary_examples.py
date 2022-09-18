270727c85f188b9c00203d91ffca7c02d432b9ea
brian huynh <huynh2102@gmail.com>
2022-09-18 23:33:33 +0700

    update code

--- python_crash_course/07-dictionary_examples.py
+++ python_crash_course/07-dictionary_examples.py
@@ -3,8 +3,21 @@
 """
 # new_dictionary = {key: value for (key,value) in iterable}
 
-items_in_cm = {'pen': 40, 'book': 50, 'keyboard': 60}
-items_in_meters = {key: value/100 for (key, value) in items_in_cm.items()}
+#items_in_cm = {'pen': 40, 'book': 50, 'keyboard': 60}
+#items_in_meters = {key: value for (key, value) in items_in_cm.items()}
+#items_in_meters = {key + ' in meters': value/100 for (key, value) in items_in_cm.items()}
 
-print(items_in_meters)
+#print(items_in_meters)
 # {'pen': 0.4, 'book': 0.5, 'keyboard': 0.6}
+
+#developers = {'Jane': 'Python', 'Jade': 'JavaScript', 'John': 'Python', 'Doe': 'JavaScript'}
+
+#python_developers = {key: value for (key, value) in developers.items() if value == 'Python'}
+
+#print(python_developers)
+
+numbers = {10: 30, 99: 20, 73: 5}
+
+item_in_num = [key for (key, value) in numbers.items() if key == 10]
+
+print(item_in_num)
